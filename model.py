964a99fb43190255a1e8c198541e3dbd8990cffe
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2018-03-27 16:07:17 +0900

    changed discriminator network

--- model.py
+++ model.py
@@ -17,24 +17,19 @@ class netD(nn.Module):
     def __init__(self):
         super(netD, self).__init__()
 
-        self.dropout = nn.Dropout(0.1)
-        self.fc1 = nn.Linear(300, 2048)
-        self.leaky_relu = nn.LeakyReLU()
-        self.fc2 = nn.Linear(2048, 1)
-        self.sigmoid = nn.Sigmoid()
-
-        weights_init(self)
-
-    def main(self, x):
-        x = self.dropout(x)
-        x = self.fc1(x)
-        x = self.leaky_relu(x)
-        x = self.fc2(x)
-        x = self.sigmoid(x)
-        return x
+        layers = [nn.Dropout((0.1))]
+        for i in range(3):
+            input_dim = 300 if i == 0 else 2048
+            output_dim = 1 if i == 2 else 2048
+            layers.append(nn.Linear(input_dim, output_dim))
+            if i < 2:
+                layers.append(nn.LeakyReLU(0.2))
+                layers.append(nn.Dropout(0.1))
+        layers.append(nn.Sigmoid())
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        return self.main(x)
+        return self.layers(x)
 
 
 class netG(nn.Module):
